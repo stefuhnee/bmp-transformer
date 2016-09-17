efb4ad8f5010a48c7177e71b035ce0ccb2c78c80
Stefanie Hansen <stefuhnee@gmail.com>
2016-09-16 22:09:36 -0700

    changed inversion function to read buffer 4 bytes at a time

--- model/color-transform.js
+++ model/color-transform.js
@@ -6,10 +6,8 @@ module.exports = exports = {};
 const Transformer = module.exports = function() { };
 
 Transformer.prototype.invert = function(callback) {
-  for (var i = 0; i < this.palette.length; i++) {
-    let currentHex = this.palette.readUInt8(i);
-    currentHex = (255 - currentHex).toString(16);
-    this.palette.writeUInt(currentHex, i);
+  for (var i = 0; i < this.palette.length; i+=4) {
+    let currentHex = this.palette.readUInt32(i);
   }
   callback(this);
   // do inversion
