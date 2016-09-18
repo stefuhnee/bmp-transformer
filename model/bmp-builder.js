ace9476a518a574f29033ac4c922e12177ec30d0
Stefanie Hansen <stefuhnee@gmail.com>
2016-09-18 16:43:42 -0700

    changed file paths, altered invert function, decided not to use a constructor for color transforms

--- model/bmp-builder.js
+++ model/bmp-builder.js
@@ -11,9 +11,11 @@ module.exports = function(buffer, filePath, transform) {
 };
 // Bitmap constructor -- from buffer, break into properties, find palette.
 const Bitmap = function(buffer) {
+  console.log(buffer);
   this.size = buffer.readUInt32LE(2);
   this.start = buffer.readUInt32LE(10);
   this.sizeOfHeader = buffer.readUInt32LE(14);
   this.colorPaletteNum = buffer.readUInt32LE(46);
   this.palette = buffer.slice(54, this.start);
+  this.wholeBuffer = buffer;
 };
