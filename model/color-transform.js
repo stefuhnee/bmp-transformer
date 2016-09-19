7f661e7919a2c445e6be0aecd044b5fd1f07705e
Stefanie Hansen <stefuhnee@gmail.com>
2016-09-19 14:42:32 -0700

    got basic grayscale function working

--- model/color-transform.js
+++ model/color-transform.js
@@ -6,29 +6,28 @@ module.exports = exports = {};
 
 // Runs a transform on the palette
 module.exports = function(filePath, transform, bitmap) {
-  // var palette = bitmap.palette;
-  // bitmap.palette = new ColorPalette;
-  // console.log('bitmap palette', bitmap.palette);
   exports[transform](bitmap, fileHelper.writeBitmap);
 };
 
-// Transform Constructor
-const ColorPalette = function() { };
-
 exports.invert = function(bitmap, callback) {
-  console.log('inverting');
-  console.log('palette', bitmap.palette);
   for (var i = 0; i < bitmap.palette.length; i++) {
-    let currentHex = bitmap.palette[i];
-    currentHex = 255 - currentHex;
+    let currentHex = bitmap.palette[i].toString(16);
+    currentHex = (255 - currentHex);
+    bitmap.palette.writeUIntLE(currentHex, i, 1);
   }
-  console.log('bitmap palette', bitmap.palette);
   callback('new', bitmap.wholeBuffer);
-  // do inversion
 };
 
-exports.grayscale = function(callback) {
-  // do grayscale
+// for every RGBA replace it with MMMA, where M = (R+G+B)/3;
+
+exports.grayscale = function(bitmap, callback) {
+  for (var i = 0; i < bitmap.palette.length; i+=4) {
+    let grayscale = bitmap.palette[i].toString(16);
+    console.log('grayscale', grayscale);
+    bitmap.palette.writeUIntLE(grayscale, i, 1);
+    bitmap.palette.writeUIntLE(grayscale, i+1, 1);        bitmap.palette.writeUIntLE(grayscale, i+2, 1);
+  }
+  callback('new', bitmap.wholeBuffer);
 };
 
 exports.randomize = function(callback) {
